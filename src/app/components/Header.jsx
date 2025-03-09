a14d901f775348c2dc8d4a179d9eea17bd2c2c29
Shahid7424 <shahshahid121212@gmail.com>
2025-03-09 16:20:26 +0530

    single dot resolve

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import WareLogo from '../utilis/Images/ware-logo.png';
-import { Menu, X, User, Search, ShoppingBag } from 'lucide-react';
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import WareLogo from "../utilis/Images/ware-logo.png";
+import { Menu, X, User, Search, ShoppingBag } from "lucide-react";
 
 const Header = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
